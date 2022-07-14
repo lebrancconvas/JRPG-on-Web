a4a4e81a5ccad014f0cb16f16414b7cd527001a4
lebrancconvas <lebrancconvas@gmail.com>
2022-07-14 22:48:59 +0700

    🗃️ Create Data for project.

---
+++ web/data/item.js
@@ -0,0 +1,5 @@
+const itemList = [
+	{name: "Potion", "effect": 10, "buff": true},
+	{name: "Super Potion", "effect": 20, buff: true},
+	{name: "Bomb", "effect": 15, buff: false}
+];
---
+++ web/data/spell.js
@@ -0,0 +1,11 @@
+const spellList = [
+	{name: "Blizzard", damage: 10},
+	{name: "Fire", damage: 10},
+	{name: "Thunder", damage: 10},
+	{name: "Blizzara", damage: 35},
+	{name: "Fira", damage: 35},
+	{name: "Thundara", damage: 35},
+	{name: "Blizzaga", damage: 50},
+	{name: "Firaga", damage: 50},
+	{name: "Thundaga", damage: 50}
+];
