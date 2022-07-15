d6d7c052f1801681264e9b82eb662e58b261a4d0
lebrancconvas <lebrancconvas@gmail.com>
2022-07-15 11:07:17 +0700

    🗃️ Create Enemy Data.

---
+++ web/data/enemy.js
@@ -0,0 +1,5 @@
+const enemyList = [
+	{name: "Black Slime", race: "Slime", hp: 50, mp: 15, avatar: ""},
+	{name: "Hopeless Graduate Student", race: "Unemployed", hp: 200, mp: 0, avatar: ""},
+	{name: "Florida Man", race: "Floridian", hp: 1000, mp: 500, avatar: ""}
+];
